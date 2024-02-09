71b6058ec532d9176d668bbf531db2d36256edfa
RanushMithila <ranushmithila45@gmail.com>
2024-02-09 19:25:47 +0530

    add tabs

--- src/app/call-analytics/call-analytics.module.ts
+++ src/app/call-analytics/call-analytics.module.ts
@@ -3,22 +3,23 @@ import { CommonModule } from '@angular/common';
 
 import { CallAnalyticsRoutingModule } from './call-analytics-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { SharedModule } from "../shared/shared.module";
+import { SharedModule } from '../shared/shared.module';
 import { StatCardComponent } from './components/stat-card/stat-card.component';
-import { PanelModule } from "primeng/panel";
+import { PanelModule } from 'primeng/panel';
 import { DoughnutChartComponent } from './components/doughnut-chart/doughnut-chart.component';
-import { ChartModule } from "primeng/chart";
+import { ChartModule } from 'primeng/chart';
 import { LineAreaChartComponent } from './components/line-area-chart/line-area-chart.component';
 import { WordCloudComponent } from './components/word-cloud/word-cloud.component';
 import { GaugeChartComponent } from './components/gauge-chart/gauge-chart.component';
-import { NgxEchartsModule } from "ngx-echarts";
+import { NgxEchartsModule } from 'ngx-echarts';
 import { HorizontalBarChartComponent } from './components/horizontal-bar-chart/horizontal-bar-chart.component';
 import { RecentCallsCardComponent } from './components/recent-calls-card/recent-calls-card.component';
 import { ButtonModule } from 'primeng/button';
-import { DataViewModule } from "primeng/dataview";
-import { TagModule } from "primeng/tag";
+import { DataViewModule } from 'primeng/dataview';
+import { TagModule } from 'primeng/tag';
 import { TooltipModule } from 'primeng/tooltip';
 import { SettingsComponent } from './components/settings/settings.component';
+import { TabViewModule } from 'primeng/tabview';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,7 @@ import { SettingsComponent } from './components/settings/settings.component';
     GaugeChartComponent,
     HorizontalBarChartComponent,
     RecentCallsCardComponent,
-    SettingsComponent
+    SettingsComponent,
   ],
   imports: [
     CommonModule,
@@ -40,10 +41,10 @@ import { SettingsComponent } from './components/settings/settings.component';
     ChartModule,
     ButtonModule,
     TooltipModule,
-    NgxEchartsModule.forRoot({echarts: () => import('echarts')}),
+    NgxEchartsModule.forRoot({ echarts: () => import('echarts') }),
     DataViewModule,
     TagModule,
-  ]
+    TabViewModule,
+  ],
 })
-export class CallAnalyticsModule {
-}
+export class CallAnalyticsModule {}
--- src/app/call-analytics/components/settings/settings.component.ts
+++ src/app/call-analytics/components/settings/settings.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styleUrl: './settings.component.scss'
+  styleUrl: './settings.component.scss',
 })
 export class SettingsComponent {
-
+  breadcrumbItems: MenuItem[] = [
+    { label: 'Call Analytics' },
+    { label: 'Settings' },
+  ];
 }
