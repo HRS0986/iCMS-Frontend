9ef640ac1796607c2d28ee1d72f1121c2ded1cea
Hirusha Fernando <heshanhfernando@gmail.com>
2024-02-10 19:44:18 +0530

    layout changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'iCMS-Frontend';
+  currentUrl = ""
+  isAuthLayout = false;
+
+  constructor(private router: Router) {
+    this.router.events
+      .subscribe((event) => {
+        if (event instanceof NavigationEnd) {
+          this.currentUrl = event.url
+          this.isAuthLayout = this.currentUrl.includes("auth");
+        }
+      })
+  }
 }
