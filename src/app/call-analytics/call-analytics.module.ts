2ab7f6acb1acd933b7685c81ff49b56f60f86fe7
RanushMithila <ranushmithila45@gmail.com>
2024-02-11 09:23:01 +0530

    form controllers added

--- src/app/call-analytics/call-analytics.module.ts
+++ src/app/call-analytics/call-analytics.module.ts
@@ -3,20 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { CallAnalyticsRoutingModule } from './call-analytics-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { SharedModule } from "../shared/shared.module";
+import { SharedModule } from '../shared/shared.module';
 import { StatCardComponent } from './components/stat-card/stat-card.component';
-import { PanelModule } from "primeng/panel";
+import { PanelModule } from 'primeng/panel';
 import { DoughnutChartComponent } from './components/doughnut-chart/doughnut-chart.component';
-import { ChartModule } from "primeng/chart";
+import { ChartModule } from 'primeng/chart';
 import { LineAreaChartComponent } from './components/line-area-chart/line-area-chart.component';
 import { WordCloudComponent } from './components/word-cloud/word-cloud.component';
 import { GaugeChartComponent } from './components/gauge-chart/gauge-chart.component';
-import { NgxEchartsModule } from "ngx-echarts";
+import { NgxEchartsModule } from 'ngx-echarts';
 import { HorizontalBarChartComponent } from './components/horizontal-bar-chart/horizontal-bar-chart.component';
 import { RecentCallsCardComponent } from './components/recent-calls-card/recent-calls-card.component';
 import { ButtonModule } from 'primeng/button';
-import { DataViewModule } from "primeng/dataview";
-import { TagModule } from "primeng/tag";
+import { DataViewModule } from 'primeng/dataview';
+import { TagModule } from 'primeng/tag';
 import { TooltipModule } from 'primeng/tooltip';
 import { CallSummaryChartComponent } from './components/call-summary-chart/call-summary-chart.component';
 import { CallRecordingsComponent } from './components/call-recordings/call-recordings.component';
@@ -28,7 +28,7 @@ import { ImageModule } from 'primeng/image';
 import { CheckboxModule } from 'primeng/checkbox';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { ChipsModule } from 'primeng/chips';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { InputSwitchModule } from 'primeng/inputswitch';
 import { InputTextModule } from 'primeng/inputtext';
 
@@ -68,6 +68,7 @@ import { InputTextModule } from 'primeng/inputtext';
     FormsModule,
     InputSwitchModule,
     InputTextModule,
+    ReactiveFormsModule,
   ],
 })
 export class CallAnalyticsModule {}
--- src/app/call-analytics/components/settings/settings.component.ts
+++ src/app/call-analytics/components/settings/settings.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { MenuItem } from 'primeng/api';
 
 @Component({
@@ -7,10 +8,34 @@ import { MenuItem } from 'primeng/api';
   styleUrl: './settings.component.scss',
 })
 export class SettingsComponent {
-  Keywords: any[] = [];
-  emails: any[] = [];
-  dir: string = '';
-  checked: boolean = true;
+  notifications = this.fb.group({
+    keywords: [],
+    emails: [],
+    bellowScore: 0,
+    aboveScore: 0,
+    aboveNotify: false,
+    bellowNotify: false,
+    checked: true,
+  });
+
+  callIntegration = this.fb.group({
+    dir: '',
+    autoDeleteStatus: false,
+    deleteAfter: 5,
+  });
+  constructor(private fb: FormBuilder) {}
+
+  onSubmit(): void {
+    console.log(this.notifications.value);
+  }
+
+  onSubmitCall(): void {
+    console.log(this.callIntegration.value);
+  }
+  // Keywords: any[] = [];
+  // emails: any[] = [];
+  // dir: string = '';
+  // checked: boolean = true;
   breadcrumbItems: MenuItem[] = [
     { label: 'Call Analytics' },
     { label: 'Settings' },
