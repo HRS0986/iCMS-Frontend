cc0884c8862fc5e6349ec94c644aa02d40fa2f7c
erandasathsara <77619152+erandasathsara@users.noreply.github.com>
2024-03-08 23:16:51 +0530

    call recording upload logic added

--- src/app/call-analytics/components/file-upload/file-upload.component.ts
+++ src/app/call-analytics/components/file-upload/file-upload.component.ts
@@ -1,53 +1,52 @@
 import { Component } from '@angular/core';
 import { MenuItem } from 'primeng/api';
+import { CallRecordingService } from '../../services/call-recording.service';
+import { finalize } from 'rxjs/operators';
 
+interface ApiResponse {
+  message: string;
+  // Add other properties as needed
+}
 
 @Component({
   selector: 'app-file-upload',
   templateUrl: './file-upload.component.html',
-  styleUrl: './file-upload.component.scss'
+  styleUrls: ['./file-upload.component.scss']
 })
 export class FileUploadComponent {
   breadcrumbItems: MenuItem[] = [
-    {label: "Call Analytics"},
-    {label: "Call Recordings"},
-    {label: "Upload Calls"}
+    { label: 'Call Analytics' },
+    { label: 'Call Recordings' },
+    { label: 'Upload Calls' }
   ];
-//  isAdvancedUpload: boolean = true;
-//   isDragover: boolean = false;
-//   droppedFiles: FileList | null = null;
 
-//   onDragOver(event: DragEvent): void {
-//     event.preventDefault();
-//     event.stopPropagation();
-//     this.isDragover = true;
-//   }
+  isUploading = false;
 
-//   onDragLeave(event: DragEvent): void {
-//     event.preventDefault();
-//     event.stopPropagation();
-//     this.isDragover = false;
-//   }
+  constructor(private fileUploadService: CallRecordingService) {}
 
-//   onDrop(event: DragEvent): void {
-//     event.preventDefault();
-//     event.stopPropagation();
-//     this.isDragover = false;
+  onFileSelected(event: any): void {
+    const files: FileList | null = event.files;
 
-//     const files = event.dataTransfer?.files || null;
-//     this.droppedFiles = files ? files : null;
-//   }
+    if (files && files.length > 0) {
+      const file: File = files[0];
+      this.isUploading = true;
 
-//   onFileChange(event: Event): void {
-//     const inputElement = event.target as HTMLInputElement;
-//     this.droppedFiles = inputElement.files;
-//   }
+      this.fileUploadService.uploadFile(file)
+        .pipe(finalize(() => this.isUploading = false))
+        .subscribe(
+          (response: ApiResponse) => {
+            console.log('File uploaded successfully', response.message);
+            // Display success message or update UI as needed
+          },
+          (error: any) => {
+            console.error('Error uploading file', error);
+            // Display error message or update UI as needed
+          }
+        );
+    } else {
+      console.warn('No file selected');
+    }
+  }
 
-//   onSubmit(): void {
-//     // Handle file upload logic here
-//     if (this.droppedFiles) {
-//       console.log("Uploading files:", this.droppedFiles);
-//       // Add your upload logic here
-//     }
-//   }
+
 }
