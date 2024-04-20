337cb121ff92907567a83e5992753e92ffb40dc5
Tharindu lakshan <lakshanapt.21@uom.lk>
2024-04-20 12:53:58 +0530

    social media  setting notification section is completed

--- src/app/social-media-analytics/social-media-analytics.module.ts
+++ src/app/social-media-analytics/social-media-analytics.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { SharedModule } from "../shared/shared.module";
 
 import { DashboardComponent } from "./pages/dashboard/dashboard.component";
@@ -14,16 +16,20 @@ import { SettingsAlerts } from "./pages/settings-alerts/settings-alerts.componen
 import { SMAnalyticsRoutingModule } from './social-media-analytics-routing.module';
 
 
+import { CheckboxModule } from 'primeng/checkbox';
+import { InputNumberModule } from 'primeng/inputnumber';
+
 
 import { TabItemComponent } from './components/tab-item/tab-item.component';
 import { TabsComponent } from './components/tabs/tabs.component';
 import { ProgressBarComponent } from './components/progress-bar/progress-bar.component';
+import { SliderModule } from 'primeng/slider';
 
-
-
+import { InputSwitchModule } from 'primeng/inputswitch';
 import { TableModule } from 'primeng/table';
 import { PanelModule } from "primeng/panel";
 import { ProgressBarModule } from 'primeng/progressbar';
+import { DropdownModule } from 'primeng/dropdown';
 // import { DoughnutChartComponent } from './components/charts/doughnut-chart/doughnut-chart.component';
 // import { ChartModule } from "primeng/chart";
 // import { LineAreaChartComponent } from './components/charts/line-area-chart/line-area-chart.component';
@@ -31,15 +37,16 @@ import { ProgressBarModule } from 'primeng/progressbar';
 // import { GaugeChartComponent } from './pages/gauge-chart/gauge-chart.component';
 // import { NgxEchartsModule } from "ngx-echarts";
 // import { HorizontalBarChartComponent } from './components/charts/horizontal-bar-chart/horizontal-bar-chart.component';
-   import { ButtonModule } from 'primeng/button';
+import { ButtonModule } from 'primeng/button';
 import { SettingsCampaignComponent } from './pages/settings-campaign/settings-campaign.component';
+import { SettingsNotificationsComponent } from './pages/settings-notifications/settings-notifications.component';
 // import { DataViewModule } from "primeng/dataview";
 // import { TagModule } from "primeng/tag";
 // import { TooltipModule } from 'primeng/tooltip';
 // import { DialogModule } from 'primeng/dialog';
 // import { BreadcrumbModule } from "primeng/breadcrumb";
-
-
+import { ChipsModule } from 'primeng/chips';
+import { TabViewModule } from 'primeng/tabview';
 // import { FileUploadModule } from 'primeng/fileupload';
 // import { ToastModule } from 'primeng/toast';
 // import { CardModule } from 'primeng/card';
@@ -64,6 +71,7 @@ import { SettingsCampaignComponent } from './pages/settings-campaign/settings-ca
     ProgressBarComponent,
     SettingsAlerts,
     SettingsCampaignComponent,
+    SettingsNotificationsComponent,
     
     
     // HorizontalBarChartComponent,
@@ -80,13 +88,20 @@ import { SettingsCampaignComponent } from './pages/settings-campaign/settings-ca
     TableModule,
     ButtonModule,
     ProgressBarModule,
-    HttpClientModule
+    HttpClientModule,
+    DropdownModule,
+    FormsModule,
+    ReactiveFormsModule,
+    SliderModule,
+    ChipsModule,
+    InputSwitchModule,
     // CardModule,
     // ToastModule,
     // TabViewModule,
     // ImageModule,
-    // CheckboxModule,
-    // InputNumberModule,
+     CheckboxModule,
+     InputNumberModule,
+     TabViewModule,
     // ChipsModule,
     // FormsModule,
     // InputSwitchModule,
