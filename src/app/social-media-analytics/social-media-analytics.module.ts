25d12f351798701fb3aeb3244c75e6a48b066ed6
Tharindu Sathsara <tharindusathsarahome@gmail.com>
2024-04-21 22:56:39 +0530

    Updated

--- src/app/social-media-analytics/social-media-analytics.module.ts
+++ src/app/social-media-analytics/social-media-analytics.module.ts
@@ -1,71 +1,40 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { SharedModule } from "../shared/shared.module";
 
-
-import { DashboardComponent } from "./pages/dashboard/dashboard.component";
-import { CAComponent } from "./pages/campaign-analysis/ca.component";
-import { PIComponent } from "./pages/platform-insights/pi.component";
-import { settingsComponent } from "./pages/settings/settings.component";
-
-
-
 import { SMAnalyticsRoutingModule } from './social-media-analytics-routing.module';
-
-import { SettingsAlerts } from "./pages/settings-alerts/settings-alerts.component";
 
 import { CheckboxModule } from 'primeng/checkbox';
 import { InputNumberModule } from 'primeng/inputnumber';
-
-
-import { TabItemComponent } from './components/tab-item/tab-item.component';
-import { TabsComponent } from './components/tabs/tabs.component';
-
- import { DoughnutChartComponent } from './components/charts/doughnut-chart/doughnut-chart.component';
-import { ProgressBarComponent } from './components/progress-bar/progress-bar.component';
 import { SliderModule } from 'primeng/slider';
-
 import { ChartModule } from 'primeng/chart';
 import { InputSwitchModule } from 'primeng/inputswitch';
 import { TableModule } from 'primeng/table';
 import { PanelModule } from "primeng/panel";
 import { ProgressBarModule } from 'primeng/progressbar';
 import { DropdownModule } from 'primeng/dropdown';
-// import { DoughnutChartComponent } from './components/charts/doughnut-chart/doughnut-chart.component';
-// import { LineAreaChartComponent } from './components/charts/line-area-chart/line-area-chart.component';
-// import { WordCloudComponent } from './pages/word-cloud/word-cloud.component';
-// import { GaugeChartComponent } from './pages/gauge-chart/gauge-chart.component';
-// import { NgxEchartsModule } from "ngx-echarts";
-import { HorizontalBarChartComponent } from './components/charts/horizontal-bar-chart/horizontal-bar-chart.component';
-// import { HorizontalBarChartComponent } from './components/charts/horizontal-bar-chart/horizontal-bar-chart.component';
 import { ButtonModule } from 'primeng/button';
-import { SettingsCampaignComponent } from './pages/settings-campaign/settings-campaign.component';
-import { SettingsNotificationsComponent } from './pages/settings-notifications/settings-notifications.component';
-// import { DataViewModule } from "primeng/dataview";
-// import { TagModule } from "primeng/tag";
-// import { TooltipModule } from 'primeng/tooltip';
-// import { DialogModule } from 'primeng/dialog';
-// import { BreadcrumbModule } from "primeng/breadcrumb";
 import { ChipsModule } from 'primeng/chips';
 import { TabViewModule } from 'primeng/tabview';
-import { SettingsThresholdsComponent } from './pages/settings-thresholds/settings-thresholds.component';
-// import { FileUploadModule } from 'primeng/fileupload';
-// import { ToastModule } from 'primeng/toast';
 import { CardModule } from 'primeng/card';
 
-// import { TabViewModule } from 'primeng/tabview';
-// import { ImageModule } from 'primeng/image';
-// import { CheckboxModule } from 'primeng/checkbox';
-// import { InputNumberModule } from 'primeng/inputnumber';
-// import { ChipsModule } from 'primeng/chips';
-// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-// import { InputSwitchModule } from 'primeng/inputswitch';
-// import { InputTextModule } from 'primeng/inputtext';
+import { DashboardComponent } from "./pages/dashboard/dashboard.component";
+import { CAComponent } from "./pages/campaign-analysis/ca.component";
+import { PIComponent } from "./pages/platform-insights/pi.component";
+import { settingsComponent } from "./pages/settings/settings.component";
+import { SettingsAlerts } from "./pages/settings-alerts/settings-alerts.component";
+import { SettingsCampaignComponent } from './pages/settings-campaign/settings-campaign.component';
+import { SettingsNotificationsComponent } from './pages/settings-notifications/settings-notifications.component';
+import { SettingsThresholdsComponent } from './pages/settings-thresholds/settings-thresholds.component';
 
+import { TabItemComponent } from './components/tab-item/tab-item.component';
+import { TabsComponent } from './components/tabs/tabs.component';
+import { DoughnutChartComponent } from './components/charts/doughnut-chart/doughnut-chart.component';
+import { ProgressBarComponent } from './components/progress-bar/progress-bar.component';
+import { HorizontalBarChartComponent } from './components/charts/horizontal-bar-chart/horizontal-bar-chart.component';
 
 
 @NgModule({
@@ -75,7 +44,6 @@ import { CardModule } from 'primeng/card';
     PIComponent,
     HorizontalBarChartComponent,
     DoughnutChartComponent,
-    //LineAreaChartComponent,
     SettingsAlerts,
     ProgressBarComponent,
     SettingsCampaignComponent,
@@ -87,11 +55,6 @@ import { CardModule } from 'primeng/card';
     SettingsCampaignComponent,
     SettingsNotificationsComponent,
     SettingsThresholdsComponent,
-    
-    
-    // HorizontalBarChartComponent,
-    // DoughnutChartComponent,
-    // LineAreaChartComponent,
   ],
   imports: [
     PanelModule,
@@ -115,27 +78,11 @@ import { CardModule } from 'primeng/card';
     TableModule,
     ButtonModule,
     ChartModule,
-    // CardModule,
-    // ToastModule,
-    // TabViewModule,
-    // ImageModule,
-     CheckboxModule,
-     InputNumberModule,
-     TabViewModule,
-    // ChipsModule,
-    // FormsModule,
-    // InputSwitchModule,
-    // InputTextModule,
-    // ReactiveFormsModule,
-    // FileUploadModule,
+    CheckboxModule,
+    InputNumberModule,
+    TabViewModule,
     ChartModule,
     ButtonModule,
-    // ButtonModule,
-    // TooltipModule,
-    // NgxEchartsModule.forRoot({echarts: () => import('echarts')}),
-    // DataViewModule,
-    // TagModule,
-    // DialogModule,
   ]
 })
 export class SMAnalyticsModule {
