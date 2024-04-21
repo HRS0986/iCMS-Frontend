bc1f7c4e5109aeb8ad6aa450b64358ebdcf7bc79
erandasathsara <77619152+erandasathsara@users.noreply.github.com>
2024-04-21 20:03:41 +0530

    call summary updated

--- src/app/call-analytics/components/call-summary-chart/call-summary-chart.component.ts
+++ src/app/call-analytics/components/call-summary-chart/call-summary-chart.component.ts
@@ -28,46 +28,35 @@ export class CallSummaryChartComponent implements OnInit {
 
   ngOnInit() {
     const documentStyle: CSSStyleDeclaration = getComputedStyle(document.documentElement);
-    this.callRecordingService.getCallsList().subscribe((data) => {
-      // Map the fetched data to match the structure of callRecordings
-      if (data.data.length === 0) {
-        this.callRecordings = [
-          {id: "cr_0", description: "Call Recording Title2", date: new Date(), duration: 3.7, operator_id: 0, sentiment: "Positive", callUrl: "dumyy url", transcription: "dummy transcription", },
-          {id: "cr_1", description: "Call Recording Title3", date: new Date(), duration: 3.7, operator_id: 0, sentiment: "Negative", callUrl: "dumyy url", transcription: "dummy transcription"},
-          {id: "cr_2", description: "Call Recording Title4", date: new Date(), duration: 3.7, operator_id: 0, sentiment: "Neutral", callUrl: "dumyy url", transcription: "dummy transcription"},
-          {id: "cr_3", description: "Call Recording Title5", date: new Date(), duration: 3.7, operator_id: 0, sentiment: "Negative", callUrl: "dumyy url", transcription: "dummy transcription"},
-          {id: "cr_4", description: "Call Recording Title6", date: new Date(), duration: 3.7, operator_id: 0, sentiment: "Positive", callUrl: "dumyy url", transcription: "dummy transcription"},
-        ];
-        console.log('Initial summaryCalls:', this.callRecordings);
-        // TODO: Ask From Eranda
-        this.noCalls = this.callRecordings.length == 0;
-      } else {
-        this.callRecordings = data.data.map((record: any) => {
-          return {
-            id: record.id,
-            description: record.description,
-            transcription: record.transcription,
-            callUrl: record.call_recording_url,
-            duration: record.call_duration ?? 4.39,
-            date: new Date(record.call_date),
-            sentiment: record.sentiment
-          } as CallRecording;
-        });
-        console.log('Fetched callRecordings:', this.callRecordings);
-      }
-
-    });
-    console.log('noCalls:', this.noCalls);
     this.statusColors = {
       "Positive": documentStyle.getPropertyValue("--positive-color"),
       "Negative": documentStyle.getPropertyValue("--negative-color"),
       "Neutral": documentStyle.getPropertyValue("--neutral-color")
     }
+    this.reloadDataSource();
   }
 
   onConfirmDelete(callId: string) {
     this.visibleConfirmation = false;
     this.deleteCall(callId);
+  }
+
+  reloadDataSource(): void {
+    this.callRecordingService.getCallsList().subscribe((data) => {
+      // Map the fetched data to match the structure of callRecordings
+      this.callRecordings = data.data.map((record: any) => {
+        return {
+          id: record.id,
+          description: record.description,
+          transcription: record.transcription,
+          callUrl: record.call_recording_url,
+          duration: record.call_duration ?? 4.39,
+          date: new Date(record.call_date),
+          sentiment: record.sentiment
+        } as CallRecording;
+      });
+      console.log('Fetched callRecordings:', this.callRecordings);
+    });
   }
 
   showDialogSummary(call: CallRecording): void {
@@ -141,28 +130,11 @@ export class CallSummaryChartComponent implements OnInit {
     this.callRecordingService.deleteCall(call_id).subscribe({
       next: (data) => {
         console.log('Delete successful', data);
-        this.refreshCallRecordings();  // Method to refresh the call recordings list
+        this.reloadDataSource();  // Method to refresh the call recordings list
       },
       error: (error) => {
         console.error('Error deleting call', error);
       }
-    });
-  }
-
-  refreshCallRecordings() {
-    this.callRecordingService.getCallsList().subscribe((data) => {
-      this.callRecordings = data.data.map((record: CallRecording) => ({
-        "title": record.description,
-        "date": new Date(record.date).toLocaleDateString(),
-        "status": record.sentiment,
-        "call_url": record.callUrl,
-        "transcription": record.transcription,
-        "duration": record.duration,
-        "call_id": record.id,
-      }));
-      console.log('Call recordings refreshed:', this.callRecordings);
-    }, error => {
-      console.error('Failed to refresh call recordings:', error);
     });
   }
 }
