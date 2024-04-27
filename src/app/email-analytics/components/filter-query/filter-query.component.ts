fa65fe2a51d301efe9ad91465c16a5fb70baec0b
WEERASINGHE RT <ravindutharuka2001@gmail.com>
2024-04-28 02:43:45 +0530

    updated filtering UI with primeNG components

--- src/app/email-analytics/components/filter-query/filter-query.component.ts
+++ src/app/email-analytics/components/filter-query/filter-query.component.ts
@@ -6,5 +6,49 @@ import { Component, Input } from '@angular/core';
   styleUrl: './filter-query.component.scss'
 })
 export class FilterQueryComponent {
-  @Input() topics!: string[];
+  dateRange: Date[] | undefined;
+  topics: string[] | undefined;
+  topicSelected: string | undefined;
+  subject: string | undefined;
+  sentiments: string[] | undefined;
+  sentimentSelected: string | undefined;
+  senders: string[] | undefined;
+  senderSelected: string | undefined | null;
+  receivers: string[] | undefined;
+  receiverSelected: string | undefined | null;
+
+  ngOnInit() {
+    this.topics = [
+        "VEGA",
+        "TravelBox",
+        "ChargeNet",
+    ];
+
+    this.sentiments = [
+      "Positive",
+      "Neutral",
+      "Negative",
+    ];
+
+    this.senders = [
+      "john@gmail.com",
+      "kate@hotmail.com",
+      "susan@yahoo.com",
+    ];
+
+    this.receivers = [
+      "vega@codegen.net",
+      "support@codegen.net",
+      "travelbox@cg.net",
+    ];
+
+  }
+  clearFilters() {
+    this.dateRange = [];
+    this.topicSelected = undefined;
+    this.subject = '';
+    this.sentimentSelected = undefined;
+    this.senderSelected = null;
+    this.receiverSelected = null;
+  }
 }
