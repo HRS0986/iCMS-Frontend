27b7e7c6c869d9a817a20241319527d5c18c969f
WEERASINGHE RT <ravindutharuka2001@gmail.com>
2024-05-29 20:35:25 +0530

    updated side nav

--- src/app/email-analytics/email-analytics-routing.module.ts
+++ src/app/email-analytics/email-analytics-routing.module.ts
@@ -25,6 +25,10 @@ const routes: Routes = [
     component: SuggestionFilteringComponent
   },
   {
+    path: "filtering",
+    component: EmailFilteringComponent
+  },
+  {
     path: "settings",
     component: SettingsComponent
   }
