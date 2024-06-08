f0e4ddae8066e6569465cd1cc110658ba3780814
WEERASINGHE RT <ravindutharuka2001@gmail.com>
2024-06-09 02:04:46 +0530

    summary card phone layout done

---
+++ src/app/email-analytics/components/dashboard-summary-card/dashboard-summary-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-summary-card',
+  templateUrl: './dashboard-summary-card.component.html',
+  styleUrl: './dashboard-summary-card.component.scss'
+})
+export class DashboardSummaryCardComponent {
+  value = 50;
+}
