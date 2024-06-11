0cf6dc09984b57bb0d890353497f84fe557b69cb
Tharindu lakshan <lakshanapt.21@uom.lk>
2024-06-11 19:15:38 +0530

    Update

--- src/app/social-media-analytics/pages/settings-notifications/settings-notifications.component.ts
+++ src/app/social-media-analytics/pages/settings-notifications/settings-notifications.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder } from '@angular/forms';
+import { MenuItem, MessageService } from 'primeng/api';
+import UserMessages from "../../../shared/user-messages";
+import { CheckboxChangeEvent } from 'primeng/checkbox';
 
 
 interface AlertType {
@@ -21,10 +24,35 @@ export class SettingsNotificationsComponent implements OnInit {
     { name: 'Email Notification' },
     { name: 'App Notification' }
   ];
+  notificationsSettingsForm = this.fb.group({
+    keywords: [],
+    emails: [],
+    bellowScore: 0,
+    aboveScore: 0,
+    aboveNotify: false,
+    bellowNotify: false,
+    checked: [true],
+  });
+  constructor(private fb: FormBuilder, private messageService: MessageService) {}
 
   selectedAlertType: AlertType | undefined;
   ngOnInit() {
-    
+    let belowAlertsEnabled =
+      this.notificationsSettingsForm.get('bellowNotify')?.value;
+    let aboveAlertsEnabled =
+      this.notificationsSettingsForm.get('aboveNotify')?.value;
+
+    if (!belowAlertsEnabled) {
+      this.notificationsSettingsForm.get('bellowScore')?.disable();
+    } else {
+      this.notificationsSettingsForm.get('bellowScore')?.enable();
+    }
+
+    if (!aboveAlertsEnabled) {
+      this.notificationsSettingsForm.get('aboveScore')?.disable();
+    } else {
+      this.notificationsSettingsForm.get('aboveScore')?.enable();
+    }
 
     this.socialMediaPlatforms = [
       { name: 'Instagram' },
@@ -32,4 +60,30 @@ export class SettingsNotificationsComponent implements OnInit {
       { name: 'Twitter' }
     ];
   }
+
+  onChangeBelowScore(event: CheckboxChangeEvent) {
+    console.log(event.checked); 
+    let belowAlertsEnabled = event.checked.length !== 0;
+    if (belowAlertsEnabled) {
+      this.notificationsSettingsForm.get('bellowScore')?.enable();
+    } else {
+      this.notificationsSettingsForm.get('bellowScore')?.disable();
+    }
+  }
+  
+  onChangeAboveScore(event: CheckboxChangeEvent) {
+    let aboveAlertsEnabled = event.checked.length !== 0;
+    if (aboveAlertsEnabled) {
+      this.notificationsSettingsForm.get('aboveScore')?.enable();
+    } else {
+      this.notificationsSettingsForm.get('aboveScore')?.disable();
+    }
+  }
+  onSubmit(): void {
+    console.log(this.notificationsSettingsForm.value);
+    this.messageService.add({ severity: 'success', summary: 'Success', detail: UserMessages.SAVED_SUCCESS })
+    console.log(this.notificationsSettingsForm.value);
+  }
 }
+
+
