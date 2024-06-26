a0099158aa1a8b2e1239e3b066c5ee7d262170cf
WEERASINGHE RT <ravindutharuka2001@gmail.com>
2024-06-26 16:42:21 +0530

    removed obsolute components
    - summary-card component
    - summary-card-container component

--- src/app/email-analytics/email-analytics-routing.module.ts
+++ src/app/email-analytics/email-analytics-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SummaryCardsContainerComponent } from './components/summary-cards-container/summary-cards-container.component';
 import { DashboardComponent } from "./components/dashboard/dashboard.component";
 import { EmailFilteringComponent } from './components/email-filtering/email-filtering.component';
 import { SettingsComponent } from './components/settings/settings.component';
