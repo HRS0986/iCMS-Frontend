6db9449e681160df05c6da7bb3158dbaa1f17506
WEERASINGHE RT <ravindutharuka2001@gmail.com>
2024-06-27 19:37:10 +0530

    changes in suggestions component

--- src/app/email-analytics/components/suggestion-filtering/suggestion-filtering.component.ts
+++ src/app/email-analytics/components/suggestion-filtering/suggestion-filtering.component.ts
@@ -1,6 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
 import { MenuItem } from 'primeng/api';
-import { DataService } from './suggestion-filtering.component.service';
 import { DataViewLazyLoadEvent, DataView } from 'primeng/dataview';
 import { SuggestionService } from '../../services/suggestion.service';
 
@@ -57,117 +56,15 @@ export class SuggestionFilteringComponent {
     newOnly: false
   }
   
-  // Ranindu's vars
-  dateRange: Date[] = [];
-  products: string[] | undefined;
-  productSelected!: string;
-  recipientEmails: string[] | undefined;
-  recipientEmailSelected!: string;
-  // ----
-
-  // first: number | undefined;
-
-  // rows: number = 10;
-
-  constructor(private dataservice: DataService, private suggestionService: SuggestionService) { }
-
-  // Ranindu's functions
-  ngOnInit() {
-    this.products = [
-        "VEGA",
-        "TravelBox",
-        "ChargeNet",
-    ];
-
-    this.recipientEmails = [
-      "readingEmail1",
-      "readingEmail2",
-      "readingEmail3",
-    ];
+  // // Ranindu's vars
+  // dateRange: Date[] = [];
+  // products: string[] | undefined;
+  // productSelected!: string;
+  // recipientEmails: string[] | undefined;
+  // recipientEmailSelected!: string;
 
 
-    this.recipientEmails = [
-      "vega@codegen.net",
-      "support@codegen.net",
-      "travelbox@cg.net",
-    ];
-    
-    // initalizing
-    this.getAllProducts()
-    this.getAllRecepientEmails()
-    this.getFilteredSuggestions(30, "", "")
-
-  }
-
-  clearFilters() {
-    this.dateRange = [];
-    this.productSelected = "";
-    this.recipientEmailSelected = "";
-    this.getFilteredSuggestions(29, this.productSelected, this.recipientEmailSelected)
-  }
-
-  applyFilters() {
-    const intervalInDays = this.calculateDatesInterval(this.dateRange)
-    this.getFilteredSuggestions(intervalInDays, this.productSelected, this.recipientEmailSelected)
-  }
-  
-  getFilteredSuggestions(intervalInDays: number, productSelected: string, recipientEmailSelected:string){
-
-    this.dataservice.getDataForFilterSuggestions( intervalInDays, productSelected, recipientEmailSelected).subscribe((data: SuggestionsData[]) => {
-      console.log("suggestions data", data)
-      this.suggestionsData= data
-      
-     });
-
-  }
-
-  getAllRecepientEmails(){
-
-    type dict = { [key: string]: any };
-
-    this.dataservice.getAllRecepients().subscribe((data: dict) => {
-      console.log("recepients data", data)
-      this.recipientEmails= data["recepients"]
-      
-     });
-
-  }
-
-    
-  getAllProducts(){
-
-    type dict = { [key: string]: any };
-
-    this.dataservice.getAllProducts().subscribe((data: dict) => {
-      console.log("products data", data)
-      this.products= data["products"]
-      
-     });
-
-  }
-
-
-  calculateDatesInterval(rangeDates: Date[]) {
-    
-    if (rangeDates.length !== 0){
-
-      const endDate = rangeDates[1]
-      const startDate = rangeDates[0]
-    
-      // Calculate the difference in milliseconds
-      const differenceMs = endDate.getTime() - startDate.getTime();
-    
-      // Convert milliseconds to days
-      const intervalInDays = differenceMs / (1000 * 3600 * 24);
-
-      return intervalInDays
-    }
-
-    else{
-      return 29
-    }
-  }
-  // ---- End of Ranindu's functions
+  constructor(private suggestionService: SuggestionService) { }
 
   loadSuggestions($event: DataViewLazyLoadEvent, criteria: Filter = this.filterCriteria) {
     this.loading = true;
