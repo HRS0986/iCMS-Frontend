1f69c909e98dc73d9e53dc7ef978197406b214b3
Wilbax <janithravisankax@gmail.com>
2024-06-28 16:06:00 +0530

    confilcts resolved

--- src/app/auth/services/authentication.service.ts
+++ src/app/auth/services/authentication.service.ts
@@ -141,10 +141,7 @@ export class AuthenticationService {
   }
 }
 
-  isAuthenticated(): boolean {
-    const currentUser = this.userPool.getCurrentUser();
-    return currentUser != null;
-  }
+
 
 
 
