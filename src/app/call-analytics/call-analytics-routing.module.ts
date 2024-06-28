a30aeef94a9976424811ff6f72c4d42923616392
Hirusha Fernando <heshanhfernando@gmail.com>
2024-06-28 09:06:02 +0530

    Added Auth Guard For Routes

--- src/app/call-analytics/call-analytics-routing.module.ts
+++ src/app/call-analytics/call-analytics-routing.module.ts
@@ -6,32 +6,39 @@ import { CallFilteringComponent } from './components/call-filtering/call-filteri
 import { SettingsComponent } from './components/settings/settings.component';
 import { FileUploadComponent } from './components/file-upload/file-upload.component';
 import { CallOperatorsComponent } from "./components/call-operators/call-operators.component";
+import {AuthGuardService} from "../shared/shared-services/auth-guard.service";
 
 
 const routes: Routes = [
   {
     path: "dashboard",
-    component: DashboardComponent
+    component: DashboardComponent,
+    canActivate: [AuthGuardService]
   },
   {
     path: "recordings",
-    component: CallRecordingsComponent
+    component: CallRecordingsComponent,
+    canActivate: [AuthGuardService]
   },
   {
     path: "filtering",
-    component: CallFilteringComponent
+    component: CallFilteringComponent,
+    canActivate: [AuthGuardService]
   },
   {
     path: 'settings',
     component: SettingsComponent,
+    canActivate: [AuthGuardService]
   },
   {
     path: "upload",
-    component: FileUploadComponent
+    component: FileUploadComponent,
+    canActivate: [AuthGuardService]
   },
   {
     path: "operators",
-    component: CallOperatorsComponent
+    component: CallOperatorsComponent,
+    canActivate: [AuthGuardService]
   }
 
 ];
