6c741775f6e583284044a152c8e63ac89e49c676
Tharindu lakshan <lakshanapt.21@uom.lk>
2024-07-02 00:35:27 +0530

    modal intergation code pushed

--- src/app/social-media-analytics/components/Modals/modal-add-new-campaign/modal-add-new-campaign.component.ts
+++ src/app/social-media-analytics/components/Modals/modal-add-new-campaign/modal-add-new-campaign.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; // Import CommonModule
+import { CommonModule } from '@angular/common';
 import { DialogModule } from 'primeng/dialog';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DropdownModule } from 'primeng/dropdown';
-
-interface platform {
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MessageService } from 'primeng/api';
+import { AlertType } from '../../../models/settings';
+import { SettingsApiService } from '../../../services/settings-api.service';
+interface Platform {
   name: string;
   icon: string;
 }
@@ -14,14 +17,15 @@ interface platform {
 @Component({
   selector: 'app-modal-add-new-campaign',
   templateUrl: './modal-add-new-campaign.component.html',
-  styleUrls: ['./modal-add-new-campaign.component.scss'], // Update styleUrls if needed
+  styleUrls: ['./modal-add-new-campaign.component.scss'],
   standalone: true,
   imports: [
-    CommonModule, // Include CommonModule here
+    CommonModule,
     DialogModule,
     ButtonModule,
     InputTextModule,
     FormsModule,
+    ReactiveFormsModule,
     DropdownModule
   ],
 })
@@ -29,18 +33,61 @@ export class ModalAddNewCampaignComponent {
   visible: boolean = false;
   postTitle: string | undefined;
   company: string | undefined;
-  platform: platform[] | undefined;
-  selectedPlatform: platform | undefined; // corrected 'selectedPlatform' spelling
-
-  constructor() {
-    this.platform = [
-      { name: 'Facebook',icon: 'assets/social-media/icons/facebook.png' },
-      { name: 'Instagram' ,icon: 'assets/social-media/icons/instargram.png'},
+  platforms: Platform[]; // Renamed from 'platform' to 'platforms'
+  selectedPlatform: Platform | undefined;
+  
+  modalAddNewSentimentForm: FormGroup;
+  
+  constructor(
+    private settingsApiService: SettingsApiService,
+    private formBuilder: FormBuilder,
+    private messageService: MessageService
+  ) {
+    this.modalAddNewSentimentForm = this.formBuilder.group({
+      platform: ['', Validators.required],
+      post_description_part: ['', Validators.required],
+      
+    });
+    
+    this.platforms = [
+      { name: 'Facebook', icon: 'assets/social-media/icons/facebook.png' },
+      { name: 'Instagram', icon: 'assets/social-media/icons/instargram.png' },
       { name: 'Twitter', icon: 'assets/social-media/icons/twitter.png' },
     ];
   }
-  topBarCaption = "Export Data";
+
   showDialog() {
     this.visible = true;
   }
+
+  onSubmitModalAddNewSentimentForm() {
+    if(this.modalAddNewSentimentForm.valid){
+      const formData = this.modalAddNewSentimentForm.value;
+      if(formData.platform.name == 'Facebook'){
+        formData.platform = 'SM01';
+      }
+      else if(formData.platform.name == 'Instagram'){
+        formData.platform = 'SM02';
+      }
+      console.log(formData);
+      this.settingsApiService.setCampaigns(formData).subscribe(
+        (response) => {
+          this.messageService.add({severity:'success', summary:'Success', detail:'Campaign added successfully'});
+          this.visible = false;
+        },
+        (error) => {
+          console.log(error);
+          this.messageService.add({severity:'error', summary:'Error', detail:'Error adding campaign'});
+        }
+      );  
+
+    }
+    else{
+      this.messageService.add({severity:'error', summary:'Error', detail:'Please fill all required fields'});
+
+    }
+
+  }
+
+
 }
--- src/app/social-media-analytics/services/settings-api.service.ts
+++ src/app/social-media-analytics/services/settings-api.service.ts
@@ -1,14 +1,17 @@
 // src\app\social-media-analytics\services\pi-api.service.ts
 
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Campaign } from '../models/campaign-analysis';
+
+const headers = new HttpHeaders()
+.set('Content-Type', 'application/json')
+.set('Accept', 'application/json');
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class SettingsApiService {
   private apiUrl = 'http://127.0.0.1:8000/social-media/settings';
 
@@ -32,9 +35,15 @@ export class SettingsApiService {
   setKeywordAlerts(data: any): Observable<any> {
     return this.http.post<any>(`${this.apiUrl}/keyword_alerts`, data);
   }
+
   setCampaigns(data: any): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/set_campaign_details`, data);
+    return this.http.post<any>('http://127.0.0.1:8000/social-media/campaign-analysis/create-campaign', data, { headers: headers });
   }
-  
+  setTopicAlerts(data: any): Observable<any> {
+    return this.http.post<any>(`http://127.0.0.1:8000/social-media/settings/add_topic_alert`, data, { headers: headers });
+  }
+  setThresold(data: any): Observable<any> {
+    return this.http.post<any>(`http://127.0.0.1:8000/social-media/settings/add_sentiment_shift_threshold`, data, { headers: headers });
+  }
 
 }
